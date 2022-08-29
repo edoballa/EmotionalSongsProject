d296bfb47d9fa099724ac26f99bb40d817f8a728
edoballa <47659738+edoballa@users.noreply.github.com>
2022-08-29 16:50:55 +0200

    Update User.java
    Add comments

--- emotionalsongs/src/main/java/objects/User.java
+++ emotionalsongs/src/main/java/objects/User.java
@@ -1,23 +1,65 @@
+/**
+* This package contains the classes that instantiate the objects.
+*
+* @author Diana Cantaluppi Matr. 744457.
+* @author Edoardo Ballabio Matr. 745115.
+*/
 package objects;
 
 import java.util.Date;
 
 public class User{
-    //(userId, username, email, password, dati anagrafici)
-
+	/**
+     * A Long to keep track of the user's id.
+     */
     private Long userId;
+    /**
+	 * A String to store the user's username.
+	 */
     private String username;
+    /**
+	 * A String to store the user's password.
+	 */
     private String password;
+    /**
+	 * A String to store the user's email.
+	 */
     private String email;
+    /**
+	 * A String to store the user's first name.
+	 */
     private String firstName;
+    /**
+	 * A String to store the user's last name.
+	 */
     private String lastName;
+    /**
+	 * A String to store the user's fiscal code.
+	 */
     private String fiscalCode;
+    /**
+	 * An Address object to store the address data.
+	 */
     private Address address;
 
+    /**
+     * User default constructor.
+     */
     public User() {
-
     }
 
+    /**
+     * User constructor with all fields as parameters.
+     * 
+     * @param userId The user's id.
+     * @param username The user's username.
+     * @param password The user's password.
+     * @param email The user's email.
+     * @param firstName The user's first name.
+     * @param lastName The user's last name.
+     * @param fiscalCode The user's fiscal code.
+     * @param address The user's address.
+     */
     public User(Long userId, String username, String password, String email, String firstName, String lastName,
             String fiscalCode, Address address) {
         super();
@@ -31,74 +73,145 @@ public class User{
         this.address = address;
     }
 
+    /**
+     * User constructor with all fields as parameters.
+     * 
+     * @param username The user's username.
+     * @param password The user's password.
+     */
     public User(String username, String password) {
         super();
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * This method return the userId field.
+     * @return Long the userId.
+     */
     public Long getUserId() {
         return userId;
     }
 
+    /**
+     * userId setter method.
+     * @param userId The user id to set.
+     */
     public void setUserId(Long userId) {
         this.userId = userId;
     }
 
+    /**
+     * This method return the username field.
+     * @return String the user's username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * username setter method.
+     * @param username The username to set.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * This method return the password field.
+     * @return String the user's password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * password setter method.
+     * @param password The password to set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * This method return the email field.
+     * @return String the user's email.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * email setter method.
+     * @param email The email to set.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * This method return the first name field.
+     * @return String the user's first name.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * firstName setter method.
+     * @param firstName The first name to set.
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * This method return the last name field.
+     * @return String the user's last name.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * lastName setter method.
+     * @param lastName The last name to set.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * This method return the fiscal code field.
+     * @return String the user's fiscal code name.
+     */
     public String getFiscalCode() {
         return fiscalCode;
     }
 
+    /**
+     * fiscalCode setter method.
+     * @param fiscalCode The fiscal code to set.
+     */
     public void setFiscalCode(String fiscalCode) {
         this.fiscalCode = fiscalCode;
     }
 
+    /**
+     * This method return the address field.
+     * @return String the user's address.
+     */
     public Address getAddress() {
         return address;
     }
 
+    /**
+     * address setter method.
+     * @param address The address to set.
+     */
     public void setAddress(Address address) {
         this.address = address;
     }
 
 }
+
