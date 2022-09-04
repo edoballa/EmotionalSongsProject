7f9986a90167828d985dbb68831acb52451ddf4f
edoballa <47659738+edoballa@users.noreply.github.com>
2022-09-04 12:09:55 +0200

    Update Address.java
    Fix comments

--- emotionalsongs/src/main/java/objects/Address.java
+++ emotionalsongs/src/main/java/objects/Address.java
@@ -1,42 +1,52 @@
 /**
 * This package contains the classes that instantiate the objects.
 *
-* @author Diana Cantaluppi Matr. 744457.
-* @author Edoardo Ballabio Matr. 745115.
+* @author Diana Cantaluppi, Matr. 744457 Sede Como.
+* @author Edoardo Ballabio, Matr. 745115 Sede Como.
 */
 package objects;
 
 
 public class Address {
 	/**
+	 * <code>address</code>
 	 * A String to store the user's address.
 	 */
     private String address;
     /**
+     * <code>addressNumber</code>
 	 * A String to store the user's addressNumber.
 	 */
     private String addressNumber;
     /**
+     * <code>cap</code>
 	 * A String to store the cap of the user's city.
 	 */
     private String cap;
     /**
+     * <code>city</code>
 	 * A String to store the user's city.
 	 */
     private String city;
     /**
+     * <code>province</code>
 	 * A String to store the user's province.
 	 */
     private String province;
     /**
+     * <code>region</code>
 	 * A String to store the user's region.
-	 
-    private String region;*/
-    /**
+	 * 
+    private String region;
+
+     * <code>country</code>
 	 * A String to store the user's country.
-	 
-    private String country;*/
+
+    private String country;
+    */
+    
     /**
+     * <code>valid</code>
 	 * A boolean to check the validity of the user's address data.
 	 */
     private boolean valid = false;
@@ -50,13 +60,13 @@ public class Address {
     /**
     * Address constructor with all fields as parameters.
     * 
-    * @param address The user's address.
-    * @param addressNumber The user's number of the address.
-    * @param cap The cap of the user's city.
-    * @param city The user's city.
-    * @param province The user's province.
-    * @param region The user's province.
-    * @param country The user's country.
+    * @param <address> The user's address.
+    * @param <addressNumber> The user's number of the address.
+    * @param <cap> The cap of the user's city.
+    * @param <city> The user's city.
+    * @param <province> The user's province.
+    * @param <region> The user's province.
+    * @param <country> The user's country.
     */
     public Address(String address, String addressNumber, String cap, String city, String province) {
         this.address = address;
@@ -79,7 +89,7 @@ public class Address {
 
     /**
      * address setter method.
-     * @param address The address to set.
+     * @param <address> The address to set.
      */
     public void setAddress(String address) {
         this.address = address;
@@ -95,7 +105,7 @@ public class Address {
 
     /**
      * addressNumber setter method.
-     * @param addressNumber The addressNumber to set.
+     * @param <addressNumber> The addressNumber to set.
      */
     public void setAddressNumber(String addressNumber) {
         this.addressNumber = addressNumber;
@@ -111,7 +121,7 @@ public class Address {
 
     /**
      * cap setter method.
-     * @param cap The cap to set.
+     * @param <cap> The cap to set.
      */
     public void setCap(String cap) {
         this.cap = cap;
@@ -127,7 +137,7 @@ public class Address {
 
     /**
      * city setter method.
-     * @param city The city to set.
+     * @param <city> The city to set.
      */
     public void setCity(String city) {
         this.city = city;
@@ -143,7 +153,7 @@ public class Address {
 
     /**
      * province setter method.
-     * @param province The province to set.
+     * @param <province> The province to set.
      */
     public void setProvince(String province) {
         this.province = province;
@@ -152,34 +162,32 @@ public class Address {
     /**
      * This method return the region field.
      * @return String the region.
-     
+
     public String getRegion() {
         return region;
-    }*/
+    }
 
-    /**
      * region setter method.
-     * @param region The region to set.
-     
+     * @param <region> The region to set.
+
     public void setRegion(String region) {
         this.region = region;
-    }*/
+    }
 
-    /**
      * This method return the country field.
      * @return String the country.
-    
+
     public String getCountry() {
         return country;
-    } */
+    }
 
-    /**
      * country setter method.
-     * @param country The country to set.
-     
+     * @param <country> The country to set.
+
     public void setCountry(String country) {
         this.country = country;
-    }*/
+    }
+    */
 
     /**
      * This method return the valid field.
