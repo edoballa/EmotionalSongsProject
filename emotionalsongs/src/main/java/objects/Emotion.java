e31f1b09fc545763a2b9fa772f903f89748d9bd4
edoballa <47659738+edoballa@users.noreply.github.com>
2022-09-04 14:03:04 +0200

    Update Emotion.java
    Fix comments

--- emotionalsongs/src/main/java/objects/Emotion.java
+++ emotionalsongs/src/main/java/objects/Emotion.java
@@ -1,21 +1,24 @@
 /**
 * This package contains the classes that instantiate the objects.
 *
-* @author Diana Cantaluppi Matr. 744457.
-* @author Edoardo Ballabio Matr. 745115.
+* @author Diana Cantaluppi, Matr. 744457 Sede Como.
+* @author Edoardo Ballabio, Matr. 745115 Sede Como.
 */
 package objects;
 
 public class Emotion {
 	/**
+	 * <code>emotionId</code>
 	 * A Long to keep track of the id of an emotion.
 	 */
     private Long emotionId;
     /**
+     * <code>name</code>
 	 * A String to store the emotion's name.
 	 */
     private String name;
     /**
+     * <code>description</code>
 	 * A String to store the emotion's description.
 	 */
     private String description;
@@ -29,9 +32,9 @@ public class Emotion {
     /**
      * Emotion constructor with all fields as parameters.
      * 
-     * @param emotionId The emotion's id.
-     * @param name The emotion's name.
-     * @param description The description of the emotion.
+     * @param <emotionId> The emotion's id.
+     * @param <name> The emotion's name.
+     * @param <description> The description of the emotion.
      */
     public Emotion(Long emotionId, String name, String description) {
         this.emotionId = emotionId;
@@ -65,7 +68,7 @@ public class Emotion {
 
     /**
      * emotionId setter method.
-     * @param emotionId The emotionId to set.
+     * @param <emotionId> The emotionId to set.
      */
     public void setEmotionId(Long emotionId) {
         this.emotionId = emotionId;
@@ -73,7 +76,7 @@ public class Emotion {
 
     /**
      * name setter method.
-     * @param name The name of the emotion to set.
+     * @param <name> The name of the emotion to set.
      */
     public void setName(String name) {
         this.name = name;
@@ -81,10 +84,9 @@ public class Emotion {
 
     /**
      * description setter method.
-     * @param description The description of the emotion to set.
+     * @param <description> The description of the emotion to set.
      */
     public void setDescription(String description) {
         this.description = description;
     }
 }
-
