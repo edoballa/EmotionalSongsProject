a9cfc2bb1f010ca6e69ee27be03b2dec2a91ddda
edoballa <47659738+edoballa@users.noreply.github.com>
2022-09-04 16:59:03 +0200

    Update Playlist_Factory.java
    Add comments

--- emotionalsongs/src/main/java/persistence/Playlist_Factory.java
+++ emotionalsongs/src/main/java/persistence/Playlist_Factory.java
@@ -1,3 +1,9 @@
+/**
+* This package contains the classes that create and manage the various factory.
+*
+* @author Diana Cantaluppi, Matr. 744457 Sede Como.
+* @author Edoardo Ballabio, Matr. 745115 Sede Como.
+*/
 package persistence;
 
 import java.io.File;
@@ -14,14 +20,41 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
+	/**
+	 * <code>FILEPATH</code>
+	 * A String to store the path of the file that contains the playlists create by the users.
+	 */
 	private static final String FILEPATH = System.getProperty("user.dir") + "\\data\\playlist_data.csv";
+	/**
+	 * <code>instance</code>
+	 * A Playlist_Factory object that allows to instantiate the class only once.
+	 */
     private static Playlist_Factory istance = null;
-    
+    /**
+	 * <code>songFactory</code>
+	 * A Song_Factory object.
+	 */
     private Song_Factory songFactory;
+    /**
+	 * <code>playlistList</code>
+	 */
     private Map<Long, Playlist> playlistMap;
+    /**
+     * A Map of Playlist that contains all the playlist create by a user.
+	 * <code>lines</code>
+	 */
     private List<String> lines;
+    /**
+	 * <code>nextKey</code>
+	 * A Long to keep track of the next key of the map.
+	 */
     private Long nextKey = null;
     
+    /**
+     * Playlist_Factory default constructor.
+     * 
+     * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+     */
     private Playlist_Factory() throws Exception {
     	this.playlistMap = new HashMap<>();
         this.lines = new ArrayList<>();
@@ -37,47 +70,97 @@ public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
         }
     }
     
+    /**
+     * This method return the istance of the Playlist_Factory object.
+     * 
+     * @return The object Playlist_Factory.
+     * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+     */
     public static Playlist_Factory getIstance() throws Exception {
     	if(istance == null){
             return new Playlist_Factory();
         } else return istance;
     }
 
+    /**
+     * This method create a playlist and add to the Map.
+     * 
+     * @param <playlist> The object that represents the playlist.
+     * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+     * @throws <IOException> This exception is thrown if there is any input/output error.
+     */
     @Override
 	public void create(Playlist playlist) throws Exception, IOException {
 		playlistMap.putIfAbsent(playlist.getPlaylistId(), playlist);
-    	save();
+    	save();		
 	}
 
+    /**
+     * This method return a Playlist based on its id.
+     * 
+     * @param <id> The playlist's id.
+     * @return The object Playlist.
+     * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+     */
 	@Override
 	public Playlist getById(Long id) throws Exception {
 		return playlistMap.get(id);
 	}
 
+	/**
+	 * This method replace in the map a Playlist object with the one passed as a parameter.
+	 * 
+	 * @param <playlist> The object that represents the playlist.
+     * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+     * @throws <IOException> This exception is thrown if there is any input/output error.
+	 */
 	@Override
 	public void update(Playlist playlist) throws Exception, IOException {
 		playlistMap.replace(playlist.getPlaylistId(), playlist);
     	save();
 	}
 
+	/**
+	 * This method remove a Playlist from the map.
+	 * 
+	 * @param <playlist> The object that represents the playlist.
+	 * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+	 */
 	@Override
 	public void delete(Playlist playlist) throws Exception {
 		playlistMap.remove(playlist.getPlaylistId());
     	save();
 	}
 	
+	/**
+	 * This method return a Map with all the Playlist.
+	 * 
+	 * @return <playlistList>
+	 */
 	@Override
 	public Map<Long, Playlist> listAll() {
 		return playlistMap;
 	}
 	
+	/**
+	 * This method return the next key of the playlist map.
+	 * 
+	 * @return An integer that indicate the next key of the map.
+	 */
 	public Long getNextKey() {
-    	while(playlistMap.containsKey(nextKey)) {
+		while(playlistMap.containsKey(nextKey)) {
     		nextKey++;
     	}
     	return nextKey;
 	}
 	
+	/**
+	 * This method returns a user's playlist based on the playlist name.
+	 * 
+	 * @param <playlistName> The name of the playlist.
+	 * @param <userId> The user's id.
+	 * @return A Playlist object.
+	 */
 	public Playlist getByName(String playlistName, Long userId) {
 		Map<Long, Playlist> userPlaylist = getUserPlaylist(userId);
 		
@@ -90,6 +173,12 @@ public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
 		return null;
 	}
 	
+	/**
+	 * This method returns all playlists created by a user.
+	 * 
+	 * @param <userId> The user's id.
+	 * @return A map of playlists.
+	 */
 	public Map<Long, Playlist> getUserPlaylist(Long userId){
 		Map<Long, Playlist> userPlaylist = new HashMap<>();
 		
@@ -102,6 +191,13 @@ public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
 		return userPlaylist;
 	}
 	
+	/**
+	 * This method creates a new file in which it saves the playlists create by a user.
+	 * If a file with that name already exists, it deletes it and creates a new one.
+	 * 
+	 * @return <strong>True</strong>, if the program successfully saves the file, <strong>False</strong>, if the program throws the exception.
+	 * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+	 */
 	private Boolean save() throws Exception{
         prepareDataForWriting();
         
@@ -120,6 +216,10 @@ public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
         return true;
     }
     
+	/**
+	 * This method adds the playlists creates by a user to a list of strings.
+	 * The data in the list are divided by a separator.
+	 */
     private void prepareDataForWriting() {
         lines.clear();
         String line = new String();
@@ -143,6 +243,10 @@ public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
         }        
     }
     
+    /**
+     * 
+     * @throws <Exception> This class indicate conditions that a reasonable application might want to catch.
+     */
     private void fillList() throws Exception{
         lines.clear();
         
@@ -153,7 +257,7 @@ public class Playlist_Factory implements IGeneric_Factory<Playlist, Long>{
         }
 
         for(String line : lines) {
-        	Playlist playlist = new Playlist(); 
+        	Playlist playlist = new Playlist();
             
             String[] strs = line.split(";");
             if(strs.length > 0){
