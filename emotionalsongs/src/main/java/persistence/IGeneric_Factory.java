63aeef0a6e873b23c1ff9e8e8321b97d59fdadf5
edoballa <47659738+edoballa@users.noreply.github.com>
2022-09-05 12:20:20 +0200

    Update IGeneric_Factory.java
    Add comments

--- emotionalsongs/src/main/java/persistence/IGeneric_Factory.java
+++ emotionalsongs/src/main/java/persistence/IGeneric_Factory.java
@@ -1,17 +1,25 @@
+/**
+* This package contains the classes that create and manage the various factory.
+*
+* @author Diana Cantaluppi, Matr. 744457 Sede Como.
+* @author Edoardo Ballabio, Matr. 745115 Sede Como.
+*/
 package persistence;
 
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * This interface contains the CRUD methods.
+ * It also contains the method to return the entire list of objects.
+ *
+ * @param <T> Generic object.
+ * @param <T1> Key type of the generic object.
+ */
 public interface IGeneric_Factory<T, T1> {
     public void create(T t) throws Exception, IOException;
     public T getById(T1 id) throws Exception;
     public void update(T t) throws Exception, IOException;
     public void delete(T t) throws Exception;
     public Map<T1, T> listAll();
-    /*public T getByUsername(String username) throws Exception;
-    public Map<Long, String> getAllUsername();
-    private Boolean save();
-    private void prepareDataForWriting();
-    private void fillCVList();*/
 }
